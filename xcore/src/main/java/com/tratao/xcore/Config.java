7fae09c751fb7a508f92be2a82eeffddb694828c
Zacks <zshaojia@gmail.com>
2022-12-07 14:48:34 +0800

    core: add config

--- xcore/src/main/java/com/tratao/xcore/Config.java
+++ xcore/src/main/java/com/tratao/xcore/Config.java
@@ -1,4 +1,41 @@
 package com.tratao.xcore;
 
 public class Config {
+    private String appKey;
+    private String privateKey;
+    private String secretKey;
+
+    public Config(String appKey) {
+        this.appKey = appKey;
+    }
+
+    public Config(String appKey, String secretKey, String privateKey) {
+        this.appKey = appKey;
+        this.secretKey = secretKey;
+        this.privateKey = privateKey;
+    }
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
 }
