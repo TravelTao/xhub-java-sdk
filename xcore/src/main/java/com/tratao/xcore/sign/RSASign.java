4981e53607cd40a828fbb8b08b2940971212792a
Zacks <zshaojia@gmail.com>
2022-12-08 15:54:06 +0800

    core: add rsa sign and verify

--- xcore/src/main/java/com/tratao/xcore/sign/RSASign.java
+++ xcore/src/main/java/com/tratao/xcore/sign/RSASign.java
@@ -1,4 +1,53 @@
 package com.tratao.xcore.sign;
 
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
 public class RSASign {
+    public static final String CHARSET = "UTF-8";
+    public static final String RSA_ALGORITHM = "RSA";
+    public static final String RSA_ALGORITHM_SIGN = "SHA256WithRSA";
+
+    /**
+     * 私钥签名
+     * @param data 需要加签的数据
+     * @param privateKeyStr 私钥字符串
+     * @return
+     */
+    public static String sign(String data,String privateKeyStr){
+        try{
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+            PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKeyStr));
+            RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(pkcs8KeySpec);
+            Signature signature = Signature.getInstance(RSA_ALGORITHM_SIGN);
+            signature.initSign(privateKey);
+            signature.update(data.getBytes(CHARSET));
+            return Base64.encodeBase64URLSafeString(signature.sign());
+        }catch(Exception e){
+            throw new RuntimeException("签名异常", e);
+        }
+    }
+
+    public static boolean verify(String data, String sign,String publicKeyStr){
+        try{
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+            X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.decodeBase64(publicKeyStr));
+            RSAPublicKey publicKey = (RSAPublicKey) keyFactory.generatePublic(x509KeySpec);
+            Signature signature = Signature.getInstance(RSA_ALGORITHM_SIGN);
+            signature.initVerify(publicKey);
+            signature.update(data.getBytes(CHARSET));
+            return signature.verify(Base64.decodeBase64(sign));
+        }catch(Exception e){
+            throw new RuntimeException("验签异常", e);
+        }
+    }
 }
