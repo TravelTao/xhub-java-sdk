6bb3f1fb42a9041d87411b8f2bbaeaae4c330c93
ZacksTsang <zshaojia@gmail.com>
2023-02-24 10:10:29 +0800

    chore: modify xcore

--- xcore/src/main/java/com/tratao/xcore/BaseClient.java
+++ xcore/src/main/java/com/tratao/xcore/BaseClient.java
@@ -77,7 +77,6 @@ public class BaseClient {
 
     public CloseableHttpClient getHttpClient() {
         TLog.getInstance().log("init httpclient.");
-        System.out.println("init httpclient.");
 
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(timeOut)
